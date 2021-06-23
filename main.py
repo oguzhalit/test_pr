bcca38d933945f85a9a7f056bc88f4354bc946bd
Oğuz <oguzhalit.sak@gmail.com>
2021-06-24 01:07:39 +0300

    add svg with changed color

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-  return "Hello World!"
+def home():
+    return render_template('home.html')
+@app.route("/about")
+def about():
+    return render_template('about.html')
---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function test(evt){
+  var svgtop=evt.target;
+  svgtop.style.fill=getRandomColor();
+}
