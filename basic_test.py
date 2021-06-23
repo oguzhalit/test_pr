9839f98756549833e5c88282745a01fe5f9d468b
Oğuz <oguzhalit.sak@gmail.com>
2021-06-24 02:31:14 +0300

    cı/cd test add bug

---
+++ basic_test.py
@@ -0,0 +1,14 @@
+from sith.main import app as flask_app
+
+
+def test_home_page():
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/' page is requested (GET)
+    THEN check that the response is valid
+    """
+
+    # Create a test client using the Flask application configured for testing
+    with flask_app.test_client() as test_client:
+        response = test_client.get('/')
+        assert response.status_code == 200
