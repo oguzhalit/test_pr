9d54e13fdd155311750e7af7bc3cd03d664826fe
Oğuz <oguzhalit.sak@gmail.com>
2021-06-24 20:45:13 +0300

    fixed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('hme.html')
+    return render_template('home.html')
 @app.route("/about")
 def about():
     return render_template('about.html')
