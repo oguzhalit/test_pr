a95336232f89c94fdae1371e2882a2ff806c995e
Oğuz <oguzhalit.sak@gmail.com>
2021-08-15 19:58:15 +0300

    mongo db test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,3 +12,19 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
+@app.route("/db")
+def database():
+    client = MongoClient('mongodb+srv://root:myFirstDatabase@cluster0.cnu3s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    db = client.test
+    
+    names = ['Oguz']
+    company = ['Yok']
+    
+    test = {
+      'name': names[0],
+      'company': company[0]
+    }
+    
+    result=db.reviews.insert_one(test)
+    
+    return f"Created {result.inserted_id}"
